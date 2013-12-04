70b191e6b9ca614c2f477e4b1cb67ad5e01613a1
Joshua Cole <josh.cole@creativelive.com>
2013-12-03 16:39:56 -0800

    Adds file opening support, thanks Chris!

--- claire.js
+++ claire.js
@@ -1,6 +1,8 @@
 (function claireBootstrap(window) {
   var path = require('path');
 
+  // Hack to initially load our plugin utilities. After this we can use requireLocal.
+  // Anything that gets required this way can use the global require normally.
   var util = require(path.join(lt.util.load.pwd, 'plugins', 'claire', 'lib', 'util'))(window);
   var requireLocal = util.requireLocal;
 
@@ -9,9 +11,9 @@
   var claireFiles = requireLocal('claire-file');
   var claire = window.claire || {};
 
-  /*\
-  |*| Claire helpers
-  \*/
+  /*************************************************************************\
+   * Claire helpers
+  \*************************************************************************/
   claire.init = function() {
     claire.$claire = $('<div id="claire"><div class="selector">');
     var $filterList = $('<div class="filter-list">').appendTo(claire.$claire.children('.selector'));
@@ -23,6 +25,7 @@
   };
 
   function setResults(err, matches) {
+    // @FIXME: Handle theoretical error condition.
     var selected = claire.$results.find('li.selected').attr('title');
     claire.$results.html('');
     claire.matches = matches;
@@ -72,19 +75,26 @@
     claireFiles.find(val, setResults, {pre: '<em>', post: '</em>', short: true});
   };
 
-  /*\
-  |*| Claire commands
-  \*/
 
+  /*************************************************************************\
+   * Claire commands
+  \*************************************************************************/
+  /*\
+  |*| Resets claire's state.
+  \*/
   claire.clear = function() {
     claire.$search.val('');
     claire.$results.html('');
   };
   util.addAction('claire.clear', claire.clear);
 
+  /*\
+  |*| Displays claire and initalizes it's context.
+  \*/
   claire.show = function() {
     var opened = util.showContainer('#bottombar');
     if(opened) {
+      util.enterContext('claire');
       claire.$search.on('keydown', processKeys);
       claire.$search.on('keyup', search);
       claire.searchRoot = util.getBufferDirectory();
@@ -93,6 +103,7 @@
       search();
 
     } else {
+      util.exitContext('claire');
       claire.$search.off('keydown', processKeys);
       claire.$search.off('keyup', search);
       claire.clear();
@@ -100,6 +111,9 @@
   };
   util.addAction('claire.show', claire.show);
 
+  /*\
+  |*| Deletes a full path component if the char under mark is a path separator, or deletes regularly.
+  \*/
   claire.smartDelete = function() {
     var val = claire.$search.val();
     if(val[val.length - 1] === '/') {
@@ -116,6 +130,9 @@
   };
   util.addAction('claire.smart-delete', claire.smartDelete);
 
+  /*\
+  |*| Iterates through search results.
+  \*/
   claire.iterate = function(mode) {
     var $selected = claire.$results.find('li.selected');
 
@@ -140,7 +157,10 @@
   };
   util.addAction('claire.iterate', claire.iterate);
 
-  // Calculate longest shared prefix or results and append to $search
+  /*\
+  |*| Calculate longest shared prefix or results and append to $search.
+  |*| Note that this is the complete path in the case of a single result.
+  \*/
   claire.complete = function() {
     var val = claireFiles.expandPath(claire.$search.val());
     var items = _.map(claire.matches, function(match) {
@@ -154,6 +174,9 @@
       claire.$search.val(items[0]);
     }
 
+    // Awesome completion trick -- compare the first and last elements of a sorted array.
+    // Their shared prefix is guaranteed to be a prefix of all elements of the array.
+    // @TODO: replace with getUnion from claire-files.
     items = items.sort();
     var first = items[0];
     var last = items[items.length - 1];
@@ -167,12 +190,33 @@
       if(shared.indexOf(val) !== -1) {
         claire.$search.val(shared);
       }
-      return;
+      return true;
     }
-
-    claire.iterate();
+    return false;
   };
   util.addAction('claire.complete', claire.complete);
+
+  /*\
+  |*| completes from the given results if possible or iterates through them if not.
+  \*/
+  claire.smartComplete = function() {
+    var completed = claire.complete();
+    if(!completed) {
+      claire.iterate();
+    }
+  }
+  util.addAction('claire.smart-complete', claire.smartComplete);
+
+  /*\
+  |*| Opens the currently selected file.
+  |*| @TODO: Should create it if it does not exist.
+  \*/
+  claire.openMatch = function() {
+    var filepath = claire.$value.val();
+    util.open(filepath);
+    claire.clear();
+  };
+  util.addAction('claire.open-match', claire.openMatch);
 
   if(!window.claire) {
     claire.init();
