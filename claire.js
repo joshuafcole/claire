c6c60b1890a8d04f839855383f03af41427f384d
Joshua Cole <joshuafcole@gmail.com>
2014-01-08 09:42:09 -0800

    Quick hack to get claire working on 6.0, major refactor en route

--- claire.js
+++ claire.js
@@ -5,7 +5,8 @@
 
   // Hack to initially load our plugin utilities. After this we can use requireLocal.
   // Anything that gets required this way can use the global require normally.
-  var util = require(path.join(lt.util.load.pwd, 'plugins', 'claire', 'lib', 'util'))(window);
+  var localRoot = path.join(lt.objs.plugins.user_plugins_dir, 'claire');
+  var util = require(path.join(localRoot, 'lib', 'util'))(window);
   var requireLocal = util.requireLocal;
 
   var _ = requireLocal('underscore');
